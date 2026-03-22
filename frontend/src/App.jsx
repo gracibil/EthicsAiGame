7de60e5139f3f45efa933cfb9bbd8769950495aa
gracjan <gracjan.bozykowski@hydryx.eu>
2026-03-22 15:57:53 +0100

    Added Stat requirments handling to options, refactored code into components, add event triggers and ending triggers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,29 @@ import EventPopUp from './components/EventPopUp'
 import StatsPopUp from './components/StatsPopUp'
 import { OptionSelectWindow } from './components/OptionSelectWIndow'
 import TextWindow from './components/textWindow'
+import { evaluateEndings } from './store/endings'
 import { useGameStore } from './store/gameStore'
+
+const INITIAL_METRICS = {
+  capital: 2,
+  compute: 2,
+  alignment: 3,
+  sentiment: 3,
+  scrutiny: 1,
+  entropy: 1,
+}
+
+const MAX_METRICS = {
+  capital: 20,
+  compute: 20,
+  alignment: 20,
+  sentiment: 20,
+  scrutiny: 20,
+  entropy: 20,
+}
+
+const METRICS_LIST = Object.keys(INITIAL_METRICS)
+
 function App() {
   const [currentScenario, setCurrentScenario] = useState(null)
   const [currentEvent, setCurrentEvent] = useState(null)
@@ -13,15 +35,67 @@ function App() {
   const [eventOpen, setEventOpen] = useState(false)
   const allScenarios = useMemo(() => Scenarios.scenarios, [])
   const allEvents = useMemo(() => Events.events, [])
+  const [gameStats, setGameStats] = useState(INITIAL_METRICS)
+
+  const updateMetrics = (key, value) => {
+      let new_value = gameStats[key] + value
+      if (new_value > MAX_METRICS[key]) {
+        // if the new value exceeds the maximum, set it to the maximum
+        new_value = MAX_METRICS[key]
+      }else if (new_value < 0) {
+        // if the new value is less than 0, set it to 0
+        new_value = 0
+      }
+
+      setGameStats((prevStats) => ({
+        ...prevStats,
+        [key]: new_value
+      }))
+  }
+
+  const checkEvents = () => {
+    for (const event of allEvents){
+      const event_triggers = event?.triggers || null
+      if (event_triggers) {
+        // check for each key value pair if the gameStats meet the trigger conditions, if they meet all conditions return the event, if not continue checking other events
+        let trigger_met = true
+
+        for (const [key, value] of Object.entries(event_triggers)) {
+          if (gameStats[key] < value) {
+            trigger_met = false
+            break
+          }
+        }
+
+        if (trigger_met) {
+          console.log('event triggered :', event)
+          allEvents.splice(allEvents.indexOf(event), 1) // remove triggered event from the list of all events to prevent it from being triggered again in the future
+          return event
+        }else{
+          return false
+        }
+
+      }
+
+      }
+  }
+
+  const checkEndings = () => {
+    // Function to check if any endings are triggered based on the current game stats
+    const ending = evaluateEndings(gameStats)
+    if (ending) {
+        //ending triggered, do something with it (e.g. show ending screen, reset game, etc.)
+        return ending
+    }
+    // no ending triggered returns false
+    return false
+
+  }
 
   const getScenario = () => {
     const nextScenarioId = currentScenario ? currentScenario.nextScenario : 0
     const nextScenario = allScenarios.find(scenario => scenario.id === nextScenarioId)
     setCurrentScenario(nextScenario)
-  const applyEffects = useGameStore((state) => state.applyEffects)
-  const activeEnding = useGameStore((state) => state.activeEnding)
-  // const metrics = useGameStore((state) => state.metrics)
-    
   }
 
   const getEvent = (eventId) =>{
@@ -40,14 +114,12 @@ function App() {
 
   const applyOptionConsequences = (option) => {
     // Apply the consequences of the selected option to the player's stats
-    if(option?.consequences?.event){
-      getEvent(option.consequences.event)
-      setEventOpen(true)
+    if (option?.consequences?.stat_effects){
+      // Apply consequences of a selected action
+      for (const [key, value] of Object.entries(option.consequences.stat_effects)) {
+          updateMetrics(key, value)
+      }
     }
-    else{
-      getScenario(currentScenario)
-    }
-    return true
   }
 
   const handleNextScenario = (option) => {
@@ -66,17 +138,26 @@ function App() {
 
   const handleOptionSelect = (option) => {
     // Handle option selection logic here
-    console.log("Selected Option:", option)
 
     checkOptionReqirments(option) // Check if the player meets the requirements for the selected option
 
     applyOptionConsequences(option) // Update player stats based on the selected option
-  }
-  const handleOptionSelect2 = (option) => {
-    applyEffects(option.effects ?? {})
 
-    if (!useGameStore.getState().activeEnding) {
-      getScenario(currentScenario)
+    const ending = checkEndings() // Check if the updated stats trigger any endings
+
+    const event = checkEvents() // Check if the updated stats trigger any events
+
+    if(ending){
+    // Handle ending logic here (e.g. show ending screen, reset game, etc.)
+    }
+    if (event){
+      // Handle event logic here (e.g. show event pop up, etc.)
+      setCurrentEvent(event)
+      setEventOpen(true)
+    }
+
+    if (!event && !ending) {
+      handleNextScenario(option) // If no events or endings are triggered, move on to the next scenario
     }
   }
 
@@ -89,39 +170,25 @@ function App() {
 
   useEffect(() => {
     if (currentScenario) {
-      console.log("Current Scenario:", currentScenario)
     }
   }, [currentScenario])
-
-  useEffect(() => {
-    if (activeEnding) {
-      
-      console.log('[Game Over]', activeEnding.id, activeEnding.title)
-      console.log(activeEnding.narrative)
-    }
-  }, [activeEnding])
 
   
   return (
     <>
       <div className={`bg-[url(./assets/images/main_screen_2.png)] flex flex-col bg-cover bg-center h-[100vh] w-[100vw] flex items-center justify-center`}>
-        <StatsPopUp playerStats={{cash: 1000, reputation: 50, entropy: 20}}/>
+        <StatsPopUp playerStats={gameStats}/>
         {
           currentEvent !== null ?(
-            <EventPopUp event={currentEvent} open={eventOpen} setOpen={setEventOpen} onOptionSelect={handleOptionSelect}  />
+            <EventPopUp event={currentEvent} open={eventOpen} gameState={gameStats} setOpen={setEventOpen} onOptionSelect={handleOptionSelect}  />
 
           ): (<></>)
         }
-     
-      
- 
-
-
           {currentScenario !== null ? (
 
           <div id='text-area' className='absolute ml-auto mr-auto w-[50%] text-white h-[75%] p-2 rounded-lg flex flex-col items-center justify-between'>
-              <TextWindow scenario={currentScenario} className={"h-3/5 p-6 bg-red-500/10"} />
-              <OptionSelectWindow gameState={{cash: 1000, reputation: 50, entropy: 20}} scenario={currentScenario} onOptionSelect={handleOptionSelect} className='w-full bg-blue-500/10 h-2/5 p-4 mb-8 flex flex-col items-center justify-center gap-2' />
+              <TextWindow scenario={currentScenario} className={"h-3/5 p-6"} />
+              <OptionSelectWindow gameState={gameStats} scenario={currentScenario} onOptionSelect={handleOptionSelect} className='w-full h-2/5 p-4 mb-8 flex flex-col items-center justify-center gap-2' />
           </div>    
 
           ) : (
--- frontend/src/components/OptionSelectWIndow.jsx
+++ frontend/src/components/OptionSelectWIndow.jsx
@@ -1,45 +1,83 @@
+import { Button } from "./ui/button"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip"
-
+import { useState } from "react"
 const OptionSelectWindow = ({ gameState, scenario, onOptionSelect, className }) => {
-    console.log("Rendering OptionSelectWindow with scenario:", scenario)
+    const [selectedOption, setSelectedOption] = useState(null)
+
+    const handleOptionClick = (option) => {
+        const hasConsequences = 'consequences' in option
+
+        if (hasConsequences && (selectedOption === null)){
+            setSelectedOption(option)
+        }else{
+            setSelectedOption(null)
+            onOptionSelect(option)
+
+        }
+
+    }
     return (
         <div className={className}>
-            {scenario.options.map((option, index) => (
-                <OptionButton index={index} gameState={gameState} option={option} onClick={() => onOptionSelect(option)} />
-            ))}
+            {
+                selectedOption !== null ? (
+                    <div className="p-4 rounded-md w-full flex flex-col bg-gray-800/90">
+                        <h3 className="text-lg font-bold mb-2">Consequences of your choice:</h3>
+                        {
+                            Object.entries(selectedOption?.consequences?.stat_effects || {}).map(([key, value]) => (
+                                <p key={key}>{`${key}: ${value > 0 ? '+' : ''}${value}`}</p>
+                            ))
+                        }
+                        <Button variant="outline" className="ml-auto mr-auto text-gray-500" onClick={() => handleOptionClick(selectedOption)}>
+                                Continue
+                        </Button>
+                        
+                    </div>
+                ) : (
+
+                    <>
+                        {scenario.options.map((option, index) => (
+                            <OptionButton key={index} index={index} gameState={gameState} option={option} onClick={() => handleOptionClick(option)} />
+                        ))}
+                    </>
+                )
+            }
+
+
         </div>
     )
 }
 
 const OptionButton = ({ index, gameState, option, onClick }) => {
+    let failedRequirements = []
     const checkOptionRequirements = () => {
         // Check if the option has requirements and if they are met
         if (option.requirements) {
-            // Implement logic to check if requirements are met
-            // This could involve checking player stats, previous choices, etc.
-            return true // Placeholder: return true if requirements are met, false otherwise
+            for (const [key, value] of Object.entries(option.requirements)) {
+                if (gameState[key] < value) {
+                    failedRequirements.push({key, value})
+                }
+            }
         }
-        return true // If there are no requirements, the option is available
-    }
+            return failedRequirements.length === 0 // All requirements met    
+        }
     const requirmentsMet = checkOptionRequirements()
-    console.log('option button rendering for option :', option)
     return (
         <>
         {
             requirmentsMet == true ? (
-                <button className=" hover:bg-green-100/10 hover:text-green-300 text-xs w-full" onClick={onClick}>
+                <button className="text-sm hover:text-blue-300 hover:text-base w-full" onClick={onClick}>
                   {index + 1}.  {option.description}
                 </button>
             ) : (
                 <TooltipProvider>
                     <Tooltip>
-                        <TooltipTrigger className='m-0'>
-                            <button className=" hover:bg-green-100/10 hover:text-green-300 text-xs w-full" disabled onClick={onClick}>
+                        <TooltipTrigger disabled className='m-0 text-gray-500 text-sm w-full'>
+                            
                                 {index+ 1} .  {option.description}
-                            </button>
+                            
                         </TooltipTrigger>
                         <TooltipContent>
-                            <p>Requirements not met</p>
+                            <p>To select this option you require: {failedRequirements.map(req => `${req.key} (${req.value})`).join(', ')}</p>
                         </TooltipContent>
                     </Tooltip>
                 </TooltipProvider>
